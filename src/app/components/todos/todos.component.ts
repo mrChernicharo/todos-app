57941795fbc4bdec4930d9cfe0f7517c3d1e8a6e
mrChernicharo <felipe.chernicharo@gmail.com>
2020-06-26 14:42:30 -0300

    todos model + render todos

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Todo } from "../../models/Todo";
 
 @Component({
-  selector: 'app-todos',
-  templateUrl: './todos.component.html',
-  styleUrls: ['./todos.component.scss']
+	selector: "app-todos",
+	templateUrl: "./todos.component.html",
+	styleUrls: ["./todos.component.scss"],
 })
 export class TodosComponent implements OnInit {
+	title: string = "My Todos";
+	todos: Todo[] = [];
 
-  constructor() { }
+	constructor() {}
 
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {
+		this.todos = [
+			{
+				id: 1,
+				title: "First Todo",
+				completed: false,
+			},
+			{
+				id: 2,
+				title: "Todo 2",
+				completed: true,
+			},
+			{
+				id: 3,
+				title: "Todo 3",
+				completed: false,
+			},
+		];
+	}
 }
