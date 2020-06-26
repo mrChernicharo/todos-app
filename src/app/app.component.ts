33123e46a6fe41b94693d79e74d5afdd6a297009
mrChernicharo <felipe.chernicharo@gmail.com>
2020-06-26 14:15:27 -0300

    testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,14 @@ import { Component } from "@angular/core";
 
 @Component({
 	selector: "app-root",
-	template: `{{ title }}`,
+	templateUrl: "app.component.html",
 	styles: [],
 })
 export class AppComponent {
 	title: string = "todos-app";
 	subtitle: string = "todo list app with Angular";
+
+	sum(a: number, b: number) {
+		return a + b;
+	}
 }
